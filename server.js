54bd83c2eb2e3c9e0dcdf9f40aa5765743031574
yozi <zzyiji@yozi.com>
2024-11-10 13:46:50 +0800

    ss

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const path = require('path');
 
 
 const app = express();
+const server = require('http').createServer(app);
 app.use(express.json());
 app.use(cookieParser());
   
@@ -154,8 +155,28 @@ app.get('/messages/:chatId', async (req, res) => {
 });
 
 
-
+const WebSocket = require('ws');
 const port = process.env.PORT || 3000;
-app.listen(3000, () => {
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', ws => {
+    console.log('New client connected');
+    ws.on('message', message => {
+        // Broadcast incoming message to all other clients
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+
+
+
+server.listen(3000, () => {
     console.log(`Server is running on port ${port}`);
 });
