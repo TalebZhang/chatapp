2bc100777a45f3dd2d44126b170ea250639c06ed
yozi <zzyiji@yozi.com>
2024-11-15 12:40:22 +0800

    conm

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const User = require('./models/User');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
+const https = require('https')
+const socketio = require('socket.io');
+const fs = require('fs');
 
 
 const app = express();
-const server = require('http').createServer(app);
+
+port=3000;
+
 app.use(express.json());
 app.use(cookieParser());
   
@@ -22,7 +27,8 @@ app.use('/uploads', express.static(path.join('/tmp')));  // Serve from /tmp fold
 
 
 
- const dbURI = process.env.MONGODB_URI;
+const dbURI = process.env.MONGODB_URI;
+//const dbURI = "mongodb://localhost:27017/mydatabase"
 
 
 
@@ -206,61 +212,6 @@ app.get('/messages', async (req, res) => {
 });
 
 
-const WebSocket = require('ws');
-const port = process.env.PORT || 3000;
-
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', ws => {
-    console.log('New client connected');
-    ws.on('message', (message) => {
-        try {
-            // Check if message is binary (Blob or ArrayBuffer)
-            if (message instanceof Buffer || message instanceof ArrayBuffer) {
-                // Send the binary data to all other clients
-                wss.clients.forEach(client => {
-                    if (client !== ws && client.readyState === WebSocket.OPEN) {
-                        client.send(message); // Send the binary data as is
-                    }
-                });
-            } else {
-                // If the message is a string (text or JSON)
-                const data = JSON.parse(message);
-
-                // Handle image messages
-                if (data.type === 'image') {
-                    // Send the image data to all other clients
-                    wss.clients.forEach(client => {
-                        if (client !== ws && client.readyState === WebSocket.OPEN) {
-                            client.send(JSON.stringify({
-                                type: 'image',
-                                data: data.data // Base64 image data
-                            }));
-                        }
-                    });
-                }
-
-                // Handle other message types (text, audio, etc.)
-                else if (data.type === 'text') {
-                    wss.clients.forEach(client => {
-                        if (client !== ws && client.readyState === WebSocket.OPEN) {
-                            client.send(JSON.stringify({
-                                type: 'text',
-                                data: data.data // Text message
-                            }));
-                        }
-                    });
-                }
-            }
-        } catch (error) {
-            console.error('Error handling WebSocket message:', error);
-        }
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-    });
-});
 
 
 // Login route
@@ -328,6 +279,136 @@ app.post('/api/auth/register', async (req, res) => {
     }
 });
 
-server.listen(port, () => {
+//we need a key and cert to run https
+//we generated them with mkcert
+// $ mkcert create-ca
+// $ mkcert create-cert
+const key = fs.readFileSync('cert.key');
+const cert = fs.readFileSync('cert.crt');
+
+//we changed our express setup so we can use https
+//pass the key and cert to createServer on https
+const expressServer = https.createServer({key, cert}, app);
+//create our socket.io server... it will listen to our express port
+const io = socketio(expressServer,{
+    cors: {
+        origin: [
+            "https://chatapp-bsrk.onrender.com",
+            // 'https://LOCAL-DEV-IP-HERE' //if using a phone or another computer
+        ],
+        methods: ["GET", "POST"]
+    }
+});
+
+//offers will contain {}
+const offers = [
+    // offererUserName
+    // offer
+    // offerIceCandidates
+    // answererUserName
+    // answer
+    // answererIceCandidates
+];
+const connectedSockets = [
+    //username, socketId
+]
+
+io.on('connection',(socket)=>{
+    // console.log("Someone has connected");
+    const userName = socket.handshake.auth.userName;
+    const password = socket.handshake.auth.password;
+
+    if(password !== "x"){
+        socket.disconnect(true);
+        return;
+    }
+    connectedSockets.push({
+        socketId: socket.id,
+        userName
+    })
+
+    //a new client has joined. If there are any offers available,
+    //emit them out
+    if(offers.length){
+        socket.emit('availableOffers',offers);
+    }
+    
+    socket.on('newOffer',newOffer=>{
+        offers.push({
+            offererUserName: userName,
+            offer: newOffer,
+            offerIceCandidates: [],
+            answererUserName: null,
+            answer: null,
+            answererIceCandidates: []
+        })
+        // console.log(newOffer.sdp.slice(50))
+        //send out to all connected sockets EXCEPT the caller
+        socket.broadcast.emit('newOfferAwaiting',offers.slice(-1))
+    })
+
+    socket.on('newAnswer',(offerObj,ackFunction)=>{
+        console.log(offerObj);
+        //emit this answer (offerObj) back to CLIENT1
+        //in order to do that, we need CLIENT1's socketid
+        const socketToAnswer = connectedSockets.find(s=>s.userName === offerObj.offererUserName)
+        if(!socketToAnswer){
+            console.log("No matching socket")
+            return;
+        }
+        //we found the matching socket, so we can emit to it!
+        const socketIdToAnswer = socketToAnswer.socketId;
+        //we find the offer to update so we can emit it
+        const offerToUpdate = offers.find(o=>o.offererUserName === offerObj.offererUserName)
+        if(!offerToUpdate){
+            console.log("No OfferToUpdate")
+            return;
+        }
+        //send back to the answerer all the iceCandidates we have already collected
+        ackFunction(offerToUpdate.offerIceCandidates);
+        offerToUpdate.answer = offerObj.answer
+        offerToUpdate.answererUserName = userName
+        //socket has a .to() which allows emiting to a "room"
+        //every socket has it's own room
+        socket.to(socketIdToAnswer).emit('answerResponse',offerToUpdate)
+    })
+
+    socket.on('sendIceCandidateToSignalingServer',iceCandidateObj=>{
+        const { didIOffer, iceUserName, iceCandidate } = iceCandidateObj;
+        // console.log(iceCandidate);
+        if(didIOffer){
+            //this ice is coming from the offerer. Send to the answerer
+            const offerInOffers = offers.find(o=>o.offererUserName === iceUserName);
+            if(offerInOffers){
+                offerInOffers.offerIceCandidates.push(iceCandidate)
+                // 1. When the answerer answers, all existing ice candidates are sent
+                // 2. Any candidates that come in after the offer has been answered, will be passed through
+                if(offerInOffers.answererUserName){
+                    //pass it through to the other socket
+                    const socketToSendTo = connectedSockets.find(s=>s.userName === offerInOffers.answererUserName);
+                    if(socketToSendTo){
+                        socket.to(socketToSendTo.socketId).emit('receivedIceCandidateFromServer',iceCandidate)
+                    }else{
+                        console.log("Ice candidate recieved but could not find answere")
+                    }
+                }
+            }
+        }else{
+            //this ice is coming from the answerer. Send to the offerer
+            //pass it through to the other socket
+            const offerInOffers = offers.find(o=>o.answererUserName === iceUserName);
+            const socketToSendTo = connectedSockets.find(s=>s.userName === offerInOffers.offererUserName);
+            if(socketToSendTo){
+                socket.to(socketToSendTo.socketId).emit('receivedIceCandidateFromServer',iceCandidate)
+            }else{
+                console.log("Ice candidate recieved but could not find offerer")
+            }
+        }
+        // console.log(offers)
+    })
+
+})
+
+expressServer.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
